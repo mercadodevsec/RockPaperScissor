d1d7f52b4dadcf1c54e06b06f5cac96e5582f2f6
mercadodevsec <mikkomercado97@gmail.com>
2026-07-25 00:56:36 -0400

    feat: refactor game logic and state management for improved functionality and leaderboard integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,45 @@
 import { useEffect, useState } from 'react'
 import './index.css'
 
+type Move = 'rock' | 'paper' | 'scissor'
+
 function App() {
   const [gameState, setGameState] = useState<'waiting' | 'playing' | 'finished'>('waiting')
   const [roundState, setRoundState] = useState<'idle' | 'countdown' | 'result'>('idle')
   const [showNameInput, setShowNameInput] = useState(true)
   const [playerName, setPlayerName] = useState('')
   const [countdown, setCountdown] = useState(3)
-  const [playerMove, setSelectedMove] = useState<'rock' | 'paper' | 'scissor' | null>(null)
-  const [computerMove, setComputerMove] = useState<'rock' | 'paper' | 'scissor' | null>(null)
+  const [playerMove, setSelectedMove] = useState<Move | null>(null)
+  const [computerMove, setComputerMove] = useState<Move | null>(null)
   const [resultMessage, setResultMessage] = useState('')
   const [playerScore, setPlayerScore] = useState(0)
   const [computerScore, setComputerScore] = useState(0)
-  const [highScore, setNewHighScore] = useState(0)
+  const [checkHighScore, setCheckHighScore] = useState(false)
   const [playerTally, setPlayerTally] = useState('')
   const [computerTally, setComputerTally] = useState('')
+  const [gameResult, setGameResult] = useState<{
+    result: {
+      playerMove: Move
+      computerMove: Move
+      outcome: 'win' | 'lose' | 'draw'
+      message: string
+      winner?: 'player' | 'computer'
+    }
+  } | null>(null)
+  const [gameLeaderboard, setGameLeaderbord] = useState("")
+
+  // start game with selected move
+  const startGame = (move: 'rock' | 'paper' | 'scissor') => {
+    setSelectedMove(move)
+    setResultMessage('')
+    setRoundState('countdown')
+    setCountdown(3)
+  }
+
+  // handle High Score Button
+  const handleHighScoreClick = async () => {
+    setCheckHighScore(true)
+  }
 
   // handle Play button click
   const handlePlayClick = () => {
@@ -31,34 +56,60 @@ function App() {
       }
     }
   }
-  // insert fetch call to backend here to get computer move and result
-  async function fetchComputerMove() {
-    try {
-      const response = await fetch(`http://localhost:3000/api/game?move=${playerMove}`)
-      const data = await response.json()
 
-      setComputerMove(data.result.computerMove)
-      setResultMessage(data.result.message)
-      if (data.result.winner === 'player') {
-        setPlayerScore((prev) => prev + 1)
-        setPlayerTally((prev) => prev + '🔵')
-      } else if (data.result.winner === 'computer') {
-        setComputerScore((prev) => prev + 1)
-        setComputerTally((prev) => prev + '🔴')
-
+  // push data leaderboard to backend
+  useEffect(() => {
+    if (gameState === 'finished') {
+      const pushLeaderbordData = async () => {
+        try {
+          await fetch(`http://localhost:3000/api/game`, {
+            method: 'POST'
+            // add proeprties
+          })
+        } catch (error) {
+          console.error('Error:', error)
+        }
       }
-    } catch (error) {
-      console.error('Error fetching move:', error)
+      pushLeaderbordData()
     }
-  }
+  }, [gameState])
 
-  // start game with selected move
-  const startGame = (move: 'rock' | 'paper' | 'scissor') => {
-    setSelectedMove(move)
-    setResultMessage('')
-    setRoundState('countdown')
-    setCountdown(3)
-  }
+  // fetch data leaderboard if user wants to view high score 
+  useEffect(async () => {
+    const fetchGameLeaderboard = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/game`)
+        const leaderboardResult = await res.json()
+        const leaderBoardResultHTML = leaderboardResult.map(score => 
+
+       ).join('')
+
+        return
+
+
+        // setGameLeaderbord(strings)
+
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    }
+
+  }, [checkHighScore])
+  // insert fetch call to backend here to get computer move and result
+  useEffect(() => {
+    // Auto-runs when playerMove changes
+    const fetchGameResultData = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/game?move=${playerMove}`)
+        const gameResult = await res.json()
+        setGameResult(gameResult)
+      }
+      catch (error) {
+        console.error('Error:', error)
+      }
+    }
+    fetchGameResultData()
+  }, [playerMove])
 
   // countdown effect
   useEffect(() => {
@@ -66,8 +117,17 @@ function App() {
       return
     }
     if (countdown === 0) {
-      if (playerMove) {
-        fetchComputerMove()
+      if (playerMove && gameResult) {
+        setComputerMove(gameResult.result.computerMove)
+        setResultMessage(gameResult.result.message)
+        if (gameResult.result.winner === 'player') {
+          setPlayerScore((prev) => prev + 1)
+          setPlayerTally((prev) => prev + '🔵')
+        } else if (gameResult.result.winner === 'computer') {
+          setComputerScore((prev) => prev + 1)
+          setComputerTally((prev) => prev + '🔴')
+
+        }
         if (playerScore >= 2 || computerScore >= 2) {
           setGameState('finished')
         }
@@ -82,27 +142,42 @@ function App() {
     return () => window.clearTimeout(timer)
   }, [countdown, roundState, playerMove])
 
-
+  if (checkHighScore === true) {
+    return (
+      <div>
+        <h1 id='title'>Rock-Paper-Scissor</h1>
+        <h2> id='core-title-display</h2>
+        <ol id='list-display'>
+          {gameLeaderboard ? (
+            <li>{printLeaderboard}</li>
+          ) : ""}
+        </ol>
+      </div>
+    )
+  }
   // render different game states
   if (gameState === 'waiting') {
     return (
       <div>
-        <h1 id="title">Rock-Paper-Scissor</h1>
-        <button id="play-btn" onClick={handlePlayClick}>Play</button>
+        <h1 id='title'>Rock-Paper-Scissor</h1>
+        <div>
+          <button id='play-btn' onClick={handlePlayClick}>Play</button>
+          <button id='view-highscore-btn' onClick={handleHighScoreClick}></button>
+        </div>
       </div>
     )
   } else if (gameState === 'playing') {
     return (
       <div>
-        <h1 id="title">Rock-Paper-Scissor</h1>
+        <h1 id='title'>Rock-Paper-Scissor</h1>
 
         {showNameInput ? (
-          <div id="user-name-container">
+          <div id='user-name-container'>
             <h2>
               Enter your name:
               <input
-                id="user-name-el"
-                type="text"
+                id='user-name-el'
+                type='text'
                 value={playerName}
                 onChange={(e) => setPlayerName(e.target.value.slice(0, 1).toUpperCase() + e.target.value.slice(1))}
                 onKeyDown={handleKeyDown}
@@ -112,34 +187,34 @@ function App() {
           </div>
         ) : (
           <>
-            <h2 id="tally-display">{playerName}: {playerTally}</h2>
-            <h2 id="tally-display">Computer: {computerTally}</h2>
+            <h2 id='tally-display'>{playerName}: {playerTally}</h2>
+            <h2 id='tally-display'>Computer: {computerTally}</h2>
             {roundState === 'countdown' ? (
               <>
-                <div id="move-dispslay-container">
-                  <h2 id="move-display">{playerMove}</h2>
+                <div id='move-dispslay-container'>
+                  <h2 id='move-display'>{playerMove}</h2>
                 </div>
-                <h2 id="countdown-display">{countdown}</h2>
+                <h2 id='countdown-display'>{countdown}</h2>
               </>
             ) : roundState === 'result' ? (
               <>
-                <div id="move-display-container">
-                  <h2 id="move-display">{playerMove}</h2>
-                  <h2 id="computer-move-display">{computerMove}</h2>
+                <div id='move-display-container'>
+                  <h2 id='move-display'>{playerMove}</h2>
+                  <h2 id='computer-move-display'>{computerMove}</h2>
                 </div>
-                <h2 id="countdown-display">{resultMessage}</h2>
-                <div id="game-btns-container">
-                  <button id="rock-btn" onClick={() => startGame('rock')}>Rock</button>
-                  <button id="paper-btn" onClick={() => startGame('paper')}>Paper</button>
-                  <button id="scissor-btn" onClick={() => startGame('scissor')}>Scissor</button>
+                <h2 id='countdown-display'>{resultMessage}</h2>
+                <div id='game-btns-container'>
+                  <button id='rock-btn' onClick={() => startGame('rock')}>Rock</button>
+                  <button id='paper-btn' onClick={() => startGame('paper')}>Paper</button>
+                  <button id='scissor-btn' onClick={() => startGame('scissor')}>Scissor</button>
                 </div>
               </>
             ) : (
               <>
-                <div id="game-btns-container">
-                  <button id="rock-btn" onClick={() => startGame('rock')}>Rock</button>
-                  <button id="paper-btn" onClick={() => startGame('paper')}>Paper</button>
-                  <button id="scissor-btn" onClick={() => startGame('scissor')}>Scissor</button>
+                <div id='game-btns-container'>
+                  <button id='rock-btn' onClick={() => startGame('rock')}>Rock</button>
+                  <button id='paper-btn' onClick={() => startGame('paper')}>Paper</button>
+                  <button id='scissor-btn' onClick={() => startGame('scissor')}>Scissor</button>
                 </div>
               </>
             )}
@@ -150,11 +225,11 @@ function App() {
   } else if (gameState === 'finished') {
     return (
       <div>
-        <h1 id="title">Rock-Paper-Scissor</h1>
+        <h1 id='title'>Rock-Paper-Scissor</h1>
         {playerScore > computerScore ? (
-          <h2 id="winner-display">{playerName} wins!</h2>
+          <h2 id='winner-display'>{playerName} wins!</h2>
         ) : (
-          <h2 id="winner-display">Computer wins!</h2>
+          <h2 id='winner-display'>Computer wins!</h2>
         )}
         <button
           onClick={() => {
@@ -166,6 +241,8 @@ function App() {
             setResultMessage('')
             setPlayerScore(0)
             setComputerScore(0)
+            setPlayerTally('')
+            setComputerTally('')
           }}
         >
           Play Again
@@ -176,5 +253,6 @@ function App() {
 
   return null
 }
+
 
 export default App
